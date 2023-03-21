2e53ddd84122eb0313c0d56eb32992c1ed515165
Manuel Faner <devcratag@gmail.com>
2023-03-21 11:31:54 -0300

    Add First and Last name to users. Add model method for Full Name. Users can now update their names from User Profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,13 @@ class User < ApplicationRecord
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
   end
 
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Anonymous"
+  end
+
   def stock_already_tracked?(ticker_symbol)
-    stock = Stock.check_db(ticker_symbol) # Isn't it unconventional to query another model from a model?
+    stock = Stock.check_db(ticker_symbol)
     return false unless stock
     stocks.where(id: stock.id).exists?
   end
