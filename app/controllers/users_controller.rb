2ace6152ed438c7776a3960ee157b3c1758d14b3
Manuel Faner <devcratag@gmail.com>
2023-03-21 15:47:50 -0300

    Users can Add or Remove friends, view their profiles and tracked stocks.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,35 @@ class UsersController < ApplicationController
   end
 
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
   def search
-    render json: params[:friend]
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 end
