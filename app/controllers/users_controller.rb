6a4e60b18dfd322b71f818789cbf9ecc9944bdf4
Manuel Faner <devcratag@gmail.com>
2023-03-21 12:10:49 -0300

    Add first and last name to user registration form. Add friends and friendships relationships.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  def my_friends
+    @friends = current_user.friends
+  end
+
   def my_portfolio
     @tracked_stocks = current_user.stocks
+  end
+
+  def search
+    render json: params[:friend]
   end
 end
